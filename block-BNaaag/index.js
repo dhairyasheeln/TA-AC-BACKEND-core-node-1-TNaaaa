1dcb31282b499f65b84cfcd10869485f705d7a4d
dhairyasheel <dhairyasheeln@gmail.com>
2022-06-08 13:49:44 +0530

    Assignment complete

---
+++ block-BNaaag/index.js
@@ -0,0 +1,9 @@
+var fs=require('fs');
+
+fs.readFile('./index.md',(err,content)=>{
+    console.log(err,content.toString());
+});
+
+var buff1=Buffer.alloc(10);
+buff1.write('Welcome to Buffer');
+console.log(buff1.toString());
