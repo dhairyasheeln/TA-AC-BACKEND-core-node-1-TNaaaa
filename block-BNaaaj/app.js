948fefde8d15c5f559982162e9d183ff5507c57a
dhairyasheel <dhairyasheeln@gmail.com>
2022-06-08 16:08:33 +0530

    Assignment Complete

---
+++ block-BNaaaj/app.js
@@ -0,0 +1,7 @@
+var area=require('./area.js');
+
+/*Area*/
+
+console.log('Area of Square:',area.square(2));
+console.log('Area of Rectabgle:',area.rectangle(3,4));
+console.log('Area of Circle',area.circle(2));
